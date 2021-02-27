a6e1edf9d28f3cff1bc1e2bca10cbf813a4f823c
DSkinner03 <73036456+DSkinner03@users.noreply.github.com>
2021-02-27 19:47:13 +0000

    General Big FIx and updates

--- DiaNet/public/Assets/Scripts/auth.js
+++ DiaNet/public/Assets/Scripts/auth.js
@@ -15,3 +15,36 @@ function LogMeIn(){
       alert(`${errorCode} \n ${errorMessage}`)
     });
 }
+
+
+function LogOut(){
+  // [START auth_sign_out]
+  firebase.auth().signOut().then(() => {
+    // Sign-out successful.
+    window.location.href = "https://dia-net.web.app"
+    document.getElementById('NewMessage').innerHTML = "Successfully Signed Out"
+  }).catch((error) => {
+    // An error happened.
+    alert("YOU ARE NOT SIGNED OUT \nContact Support Immediately.")
+  });
+  // [END auth_sign_out]
+}
+
+
+function authStateListener() {
+  // [START auth_state_listener]
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      var uid = user.uid;
+      // ...
+    } else {
+      // User is signed out
+      // ...
+      window.location.href = "https://dia-net.web.app"
+      document.getElementById('NewMessage').innerHTML = "You are not Logged in."
+    }
+  });
+  // [END auth_state_listener]
+}
